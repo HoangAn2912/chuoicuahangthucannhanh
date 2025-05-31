9a71c7f1fe024954f68451b4fc595fde42b931ea
code <hoangan2912.npha@gmail.com>
2025-05-31 10:24:24 +0700

    Gỡ repo con và thêm lại thư mục thường

---
+++ chuoithucannhanh/views/trangchu/index.php
@@ -0,0 +1,24 @@
+<!-- Sidebar -->
+<?php
+    if($_SESSION["dangnhap"] == 'nvb'){
+        require("layout/navnvb.php");
+    }elseif($_SESSION["dangnhap"] == 'nvbh'){
+        require("layout/navnvbh.php");
+    }else{
+        require("layout/navqlchuoi.php");
+    }
+?>
+<div class="sidebar">
+        <a href="#"><i class="fas fa-cogs"></i> Cài đặt</a>
+        <a href="#"><i class="fas fa-question-circle"></i> Hỗ trợ</a>
+    </div>
+
+    <div style="margin-left: 210px; padding: 20px;">
+        <h2>Nội dung chính</h2>
+        <p>Đây là nơi nội dung của trang sẽ xuất hiện...</p>
+        <p>thu nghiem
+        </p>
+    </div>
+
+</body>
+</html>
